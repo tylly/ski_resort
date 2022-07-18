73a6c3a924549566ac78db312d05393c4fb12cca
jamestylladams <jamestylladams@gmail.com>
2022-07-18 14:43:07 -0600

    prettier

--- controller/region_routes.js
+++ controller/region_routes.js
@@ -5,7 +5,6 @@ const Region = require("../models/region");
 const axios = require("axios");
 const Resort = require("../models/resort");
 
-
 router.get("/", async (req, res) => {
   try {
     res.render("regions/index");
@@ -16,53 +15,53 @@ router.get("/", async (req, res) => {
 
 //DELETE region
 router.delete("/delete/:regionName", async (req, res) => {
-    destroy = req.body.regionId
-    await Region.deleteOne({
-        $and: [{owner: req.session.userId}, { regionName: destroy }]
-    })
-    res.redirect("http://localhost:3000/resorts/home");
+  destroy = req.body.regionId;
+  await Region.deleteOne({
+    $and: [{ owner: req.session.userId }, { regionName: destroy }],
   });
+  res.redirect("http://localhost:3000/resorts/home");
+});
 
 router.post("/", async (req, res) => {
-    console.log(req.body)
-    const newRegion = {
-      regionName: regionId,
-      owner: req.session.userId
-    };
-    Region.create(newRegion);
-    res.redirect("resorts/home");
-  });
+  console.log(req.body);
+  const newRegion = {
+    regionName: regionId,
+    owner: req.session.userId,
+  };
+  Region.create(newRegion);
+  res.redirect("resorts/home");
+});
 
 router.get("/new", async (req, res) => {
-    try {
-      res.render("regions/new");
-    } catch {
-      console.log("error");
-    }
-  });
+  try {
+    res.render("regions/new");
+  } catch {
+    console.log("error");
+  }
+});
 
-  router.post("/new", async (req, res) => {
-    resp = await axios.get(
-      `http://feeds.snocountry.net/getSnowReport.php?apiKey=SnoCountry.example&regions=${req.body.regionId}`
-    );
-        
-    regions = resp.data.items
-    regionId = req.body.regionId
-    regionId = regionId.toUpperCase()
-    console.log(req.body)
+router.post("/new", async (req, res) => {
+  resp = await axios.get(
+    `http://feeds.snocountry.net/getSnowReport.php?apiKey=SnoCountry.example&regions=${req.body.regionId}`
+  );
 
-    res.render("regions/view", { regions, regionId });
-  });
+  regions = resp.data.items;
+  regionId = req.body.regionId;
+  regionId = regionId.toUpperCase();
+  console.log(req.body);
 
-  //SHOW for User Regions
+  res.render("regions/view", { regions, regionId });
+});
 
-  router.get('/show/:regionId', async (req, res) => {
-      regionId = req.params.regionId
-      resp = await axios.get(
-        `http://feeds.snocountry.net/getSnowReport.php?apiKey=SnoCountry.example&regions=${regionId}`
-      );
-      regions = resp.data.items
-      res.render('regions/show', {regions, regionId})
-  })
+//SHOW for User Regions
+
+router.get("/show/:regionId", async (req, res) => {
+  regionId = req.params.regionId;
+  resp = await axios.get(
+    `http://feeds.snocountry.net/getSnowReport.php?apiKey=SnoCountry.example&regions=${regionId}`
+  );
+  regions = resp.data.items;
+  res.render("regions/show", { regions, regionId });
+});
 
 module.exports = router;
