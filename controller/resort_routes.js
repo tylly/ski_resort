cbae5e42807b9364dae13825ee6a0b2bc9cd8e57
jamestylladams <jamestylladams@gmail.com>
2022-07-20 08:39:52 -0600

    added seed route

--- controller/resort_routes.js
+++ controller/resort_routes.js
@@ -203,4 +203,88 @@ router.get("/show/:resortId", async (req, res) => {
   }
 });
 
+// seed route --> has been moved to models/seed.js
+// insert many items into our database with just going to this route
+// localhost:3000/fruits/seed
+router.get('/seed', (req, res) => {
+//     // starting data
+const startStates = [
+  { name: "Alabama", code: "AL"},
+  { name: "Alaska", code: "AK"},
+  { name: "Arizona", code: "AZ"},
+  { name: "Australia", code: "AU"},
+  { name: "Austria", code: "AT" },
+  { name: "Arkansas", code: "AR"},
+  { name: "California", code: "CA"},
+  { name: "Colorado", code: "CO"},
+  { name: "Connecticut", code: "CT"},
+  { name: "Delaware", code: "DE"},
+  { name: "Florida", code: "FL"},
+  { name: "France", code: "FR"},
+  { name: "France", code: "FRA"},
+  { name: "Georgia", code: "GA"},
+  { name: "Germany", code: "DEU"},
+  { name: "Germany", code: "DE"},
+  { name: "Germany", code: "GER"},
+  { name: "Hawaii", code: "HI"},
+  { name: "Idaho", code: "ID"},
+  { name: "Illinois", code: "IL"},
+  { name: "Indiana", code: "IN"},
+  { name: "Iowa", code: "IA"},
+  { name: "Italy", code: "IT"},
+  { name: "Italy", code: "ITA"},
+  { name: "Kansas", code: "KA"},
+  { name: "Kentucky", code: "KY"},
+  { name: "Louisiana", code: "LA"},
+  { name: "Maine", code: "ME"},
+  { name: "Maryland", code: "MD"},
+  { name: "Massachusetts", code: "MA"},
+  { name: "Michigan", code: "MI"},
+  { name: "Minnesota", code: "MN"},
+  { name: "Mississippi", code: "MS"},
+  { name: "Missouri", code: "MO"},
+  { name: "Montana", code: "MT"},
+  { name: "Nebraska", code: "NE"},
+  { name: "Nevada", code: "NV"},
+  { name: "New Hampshire", code: "NH"},
+  { name: "New Jersey", code: "NJ"},
+  { name: "New Mexico", code: "NM"},
+  { name: "New York", code: "NY"},
+  { name: "New Zealand", code: "NZ"},
+  { name: "North Carolina", code: "NC"},
+  { name: "North Dakota", code: "ND"},
+  { name: "Ohio", code: "OH"},
+  { name: "Oklahoma", code: "OK"},
+  { name: "Oregon", code: "OR"},
+  { name: "Pennsylvania", code: "PA"},
+  { name: "Rhode Island", code: "RI"},
+  { name: "South Carolina", code: "SC"},
+  { name: "South Dakota", code: "SD"},
+  { name: "Switzerland", code: "SW"},
+  { name: "Switzerland", code: "SUI"},
+  { name: "Tennessee", code: "TN"},
+  { name: "Texas", code: "TX"},
+  { name: "Utah", code: "UT"},
+  { name: "Vermont", code: "VT"},
+  { name: "Virginia", code: "VA"},
+  { name: "Washington", code: "WA"},
+  { name: "West Virginia", code: "WV"},
+  { name: "Wisconsin", code: "WI"},
+  { name: "Wyoming", code: "WY"},
+];
+  // delete if we have fruits
+  State.deleteMany({})
+		// insert data
+		.then(() => {
+            State.create(startStates)
+            // return this data as json to view
+            .then(data => {
+                res.json(data)
+                console.log('this worked')
+            })
+            // .catch(err => console.error(err))
+		    .catch(console.error)
+        })
+})
+
 module.exports = router;
